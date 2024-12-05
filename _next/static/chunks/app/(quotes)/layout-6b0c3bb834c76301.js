e8431ac8fd4bf019bc0f05fabf4d8bf3ce66da27
zelonKim <ksz18601@gmail.com>
2024-12-05 16:20:24 +0900

    Deploy

---
+++ _next/static/chunks/app/(quotes)/layout-6b0c3bb834c76301.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[61],{5394:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,1941,23)),Promise.resolve().then(n.t.bind(n,5319,23)),Promise.resolve().then(n.t.bind(n,533,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[630,747,723,700,744],()=>s(5394)),_N_E=e.O()}]);
